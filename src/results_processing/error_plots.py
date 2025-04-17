fec3c596c652b1654fe475f4bd1cde1b4bbeffeb
Martin Barry <martin.barry@hevs.ch>
2025-04-17 13:37:10 +0200

    update plot errors

--- src/results_processing/error_plots.py
+++ src/results_processing/error_plots.py
@@ -2,19 +2,28 @@ from config import results_path
 import os
 import matplotlib.pyplot as plt
 
-files = ["slurm-627.out", "slurm-624.out"]
+files = ["training_error.txt"]
+def find_loss(text):
+    text = text.split(",")
+    for subtext in text:
+        if "Loss:" in subtext:
+            return float(subtext.split(":")[-1].strip())
 for filename in files:
+    print(os.path.join(results_path,filename))
     with open(os.path.join(results_path,filename) , 'r') as file:
         lines = file.readlines()
 
-    errors = [line for line in lines if "Batch Step" in line]
-    epoch_errors = [line for line in lines if "Epoch" in line]
+    errors = [line for line in lines if "Batch" in line]
 
-    error_values = [float(text.split("Loss:")[-1].strip()) for text in errors]
-    training_errors = [error  for error in error_values if error < 0.1]
-    validation_errors = [error for error in error_values if error >= 0.1]
+    error_values = [find_loss(text) for text in errors]
     # plt.plot(training_errors, label=f'Training Errors {filename}')
-    plt.plot(validation_errors, label=f'Validation Errors {filename}')
+    ## temporal smoothing of the error
+    window = 2
+    error_values = [error_values[i] for i in range(len(error_values)) if i % window == 0]
+    error_values = [sum(error_values[i:i+window])/window for i in range(len(error_values)-window)]
+    error_values = error_values[:len(error_values)-window]
+
+    plt.plot(error_values, label=f'{filename.split("_")[0]}')
 plt.legend()
 plt.xlabel('Batch Step')
 plt.ylabel('Error')
